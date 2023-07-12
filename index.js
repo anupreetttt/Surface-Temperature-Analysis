d9c450f244dcb36e9636adf74c40dd2034a46c2b
Anupreet Paulkar <apaulk2@uic.edu>
2023-07-12 00:08:41 -0500

    Bar chart showing

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+const xLabels = [];
+const yTemps = [];
+chartIt();
+
+async function chartIt() {
+    await fetchData();
+    const ctx = document.getElementById('chart');
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: xLabels,
+        datasets: [{
+          label: 'Global Avg Temp',
+          data: yTemps,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  
+}
+
 async function fetchData() {
     const response = await fetch('ZonAnn.Ts+dSST.csv');
 
@@ -9,10 +37,11 @@ async function fetchData() {
     table.forEach(row => {
         const columns = row.split(',');
         const year = columns[0];
+        xLabels.push(year);
         const temp = columns[1];
+        yTemps.push(parseFloat(temp) + 14);
 
         console.log(year, temp);
     })
 }
 
-fetchData();
